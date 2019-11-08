dd17d0d0442c42b2cd7d1aa134d369f4f42ac470
shashi <shashidharavtc@gmail.com>
2019-11-08 08:29:37 +0530

    Writing and reading json to file

---
+++ playground/fs/jsonHandler.js
@@ -0,0 +1,31 @@
+
+var fs = require('fs');
+
+var tokenObj = {
+	user: "secret",
+	root: "supersecret",
+	publ: "nopass"
+};
+
+var json = JSON.stringify(tokenObj);
+
+fs.writeFile('myjsonfile.json', json, 'utf8', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
+
+fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it is an object
+	
+	var userPass = obj.user;
+	var suPass = obj.root;
+	var publPass = obj.publ;
+	
+	console.log("user" + userPass);
+//If you want to rewrite data, do below steps
+    //json = JSON.stringify(obj);
+    //fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back 
+}});
